f4cd61b530acc9b1dce36941cf30adddc69fbb55
Mr-SteveNascimento <stevecraken@gmail.com>
2019-07-30 17:56:38 -0300

    Finalizando a adição de funções

--- Main.py
+++ Main.py
@@ -1,21 +1,8 @@
 
-
-# Implementação de um simulador de memória cache po Steve Nascimento
-# 
-# Mapeamento direto				
-# 
-# Política de Substituição FIFO
-#
-# RAM de 96 bytes ou 768 bits (24 Blocos de 4 linhas de 1 byte)
-#
-# CACHE de 32 bytes (8 Linhas de 32 bits)
-
-
-
-
-
-
-
+# Implementação de um simulador de memória CACHE por Steve Nascimento
+# 			
+# CACHE de 192 bytes (96 Linhas de 8 bits)
+import time
 		
 
 def main():
@@ -25,48 +12,46 @@ def main():
 	print("ATENÇÂO")
 	print("Ao inserir um arquivo no simulador, certifique-se que os espaçamentos estão corretos")
 	print("Exemplo:")
-	print("INSTRUCOES TIPO R: 'add $1,$2,$3'")
-	print("INSTRUCOES TIPO I: 'addi $1,$2,10'")
-	print("INSTRUCOES TIPO J: 'j 15'")
+	print("INSTRUCOES TIPO ADD: 'add $1,$2,$3'")
+	print("INSTRUCOES TIPO ADDI: 'addi $1,$2,10'")
+	print("INSTRUCOES TIPO J: 'j LABEL'")
+	print("INSTRUCOES TIPO LABEL: 'LABEL:'")
+	print("INSTRUCOES TIPO BEQ: 'beq $3,$4,L1'")
+	print("INSTRUCOES TIPO BNE: 'bne $3,$4,L1'")
 	print()
 	nome = str(input("Insira a seguir o nome do arquivo em cógido assembly com extensão: "))
 
-	memorias = ramCacheRegistersGenerate()
+	memorias = cacheRegistersGenerate()
 	exception = inicializacaoDoArquivo(nome)
 
 	if (exception == 0):
 		return 0
 	else:
 		REG = memorias[0]
-		RAM = memorias[1]
-		CACHE = memorias[2]
+		CACHE = memorias[1]
 		INSTRUCOES = exception
 
-		calculo(REG,RAM,CACHE,INSTRUCOES)
+		calculo(REG,CACHE,INSTRUCOES)
+	
+	return 0
 
-def ramCacheRegistersGenerate():
+def cacheRegistersGenerate():
 
 	# Registers 
 
 	REG = [[],[]]
 	for i in range(8):
-		REG[0].append([])
+		REG[0].append("0")
 	for i in range(10):
-		REG[1].append([])
-
-	# RAM
-
-	RAM = []
-	for i in range(96):
-		RAM.append(["00000000"])
+		REG[1].append("0")
 
 	# CACHE
 
 	CACHE = []
-	for i in range(8):
-		CACHE.append(["00000000000000000000000000000000"])
+	for i in range(96):
+		CACHE.append("0")
 
-	return ([REG,RAM,CACHE])
+	return ([REG,CACHE])
 
 def inicializacaoDoArquivo(nome):
 
@@ -91,24 +76,29 @@ def inicializacaoDoArquivo(nome):
 def imprimirMatriz(res,inst):
 	
 	print()
-	print("REGISTRADORES")
 	print()
-	print("----------------------------")
-	print("    REG     |     VALOR     ")
-	print("----------------------------")
+	print()
+	print()
+	print("      REGISTRADORES")
+	print()
+	print("      ----------------------------")
+	print("      |   REG     |     VALOR    |")
+	print("      ----------------------------")
 	for i in range(8):
-		print("    $"+ str(i)+"    |    "+str(res[0][i])+"    ")
+		print(("      |    ${0}     |       {1}      |").format(i, res[0][i]))
 	for i in range(10):
-		print("    t"+ str(i)+"    |    "+str(res[1][i])+"    ")
-	print("----------------------------")	
+		print(("      |    t{0}     |       {1}      |").format(i, res[1][i]))
+	print("      ----------------------------")	
 	print()
-	print("INSTRUÇỖES")
+	print("     INSTRUÇÕES")
 	print()
 	for i in range(len(inst)):
-		print(inst[i])
+		print(("     {}").format(inst[i]))
 	print()
 	print()
 	print("*********************************************************************")
+
+	return 0
 
 def conversorDeInstrucoes(lista):
 
@@ -118,30 +108,97 @@ def conversorDeInstrucoes(lista):
 	total = len(lista)
 	i = 0
 	while i < total:
-		if(lista[i]  == ""):
+		if(lista[i]  == "" or lista[i] == " "):
 			del(lista[i])
 			total-=1
-		i+=1
+		else:
+			i+=1
+
 	return(lista)
 
-def calculo(REG,RAM,CACHE,INSTRUCOES):
+def calculo(REG,CACHE,INSTRUCOES):
 	resultados = REG
-	for i in range(len(INSTRUCOES)):
-
-		#Mostra a linha que está sendo executada
+	imprimirMatriz(REG,INSTRUCOES)
+	branch = 0
+	i = 0
+	laco = 0
+	REG[0][1] = "5"
+	REG[0][0] = "1"
+	while (i < len(INSTRUCOES)):
+		if (laco == 0):
+			time.sleep(10)
+			laco = 1
+		else:
+			time.sleep(3)
+		atual = INSTRUCOES[i]
 		INSTRUCOES[i] += "*"
-		if (INSTRUCOES[i][:3] == 'add'):
-			resultados = add(resultados)
+
+		if (INSTRUCOES[i][0] == " "):
+			print("Remova o espaço no início da seguinte instrução!!!")
+			INSTRUCOES[i] = INSTRUCOES[i].replace("*", "")
+			print(("{} na linha {}").format(INSTRUCOES[i], i+1))
+			break
+			
 		elif (INSTRUCOES[i][:4] == "addi"):
-			resultados = addi(resultados)
+			resultados = addi(resultados,atual)
+			if (resultados == 0):
+				break
+			imprimirMatriz(resultados,INSTRUCOES)
+
+		elif (INSTRUCOES[i][:3] == 'add'):
+			resultados = add(resultados,atual)
+			if (resultados == 0):
+				break
+			imprimirMatriz(resultados,INSTRUCOES)
+
 		elif (INSTRUCOES[i][:2] == "lw"):
-			resultados = lw(resultados)
-		elif (INSTRUCOES[i][0] == "j"):		
-			resultados = j(resultados)
+			resultados = lw(resultados,CACHE,atual)
+			if (resultados == 0):
+				break
+			imprimirMatriz(resultados,INSTRUCOES)
+
+		elif (INSTRUCOES[i][:2] == "sw"):
+			resultados = sw(resultados,CACHE,atual)
+			if (resultados == 0):
+				break
+			imprimirMatriz(resultados,INSTRUCOES)
+
+		elif (atual[-1] == ":"):
+			imprimirMatriz(resultados,INSTRUCOES)
+			INSTRUCOES[i] = INSTRUCOES[i].replace("*", "")
+			i+=1
+			continue
+
+		elif (INSTRUCOES[i][0] == "j"):
+			INSTRUCOES[i] = INSTRUCOES[i].replace("*", "")
+			branch = j(atual,INSTRUCOES)
+			if (branch == "Erro"):
+				print("Seu jumper não funcionou, verifique se o label está correto!!!")
+				print(("{} na linha {}").format(INSTRUCOES[i], i+1))
+				break
+			INSTRUCOES[i] += "*"
+			imprimirMatriz(resultados,INSTRUCOES)
+
 		elif (INSTRUCOES[i][:3] == "bne"):
-			resultados = bne(resultados)
+			INSTRUCOES[i] = INSTRUCOES[i].replace("*", "")
+			branch = bne(atual,resultados,INSTRUCOES)
+			if (branch == "Erro"):
+				print("Seu branch if not equals não funcionou, verifique se o label está correto!!!")
+				print(("{} na linha {}").format(INSTRUCOES[i], i+1))
+				break
+			INSTRUCOES[i] += "*"
+			imprimirMatriz(resultados,INSTRUCOES)
+
 		elif (INSTRUCOES[i][:3] == "beq"):
-			resultados = beq(resultados)
+			INSTRUCOES[i] = INSTRUCOES[i].replace("*", "")
+			branch = beq(atual,resultados,INSTRUCOES)
+			if (branch == "Erro"):
+				print("Seu branch if equals não funcionou, verifique se o label está correto!!!")
+				print(("{} na linha {}").format(INSTRUCOES[i], i+1))
+				break
+			INSTRUCOES[i] += "*"
+			imprimirMatriz(resultados,INSTRUCOES)
+
 		else:
 			print("Esta seguinte instrução não foi reconhecida!!!")
 			INSTRUCOES[i] = INSTRUCOES[i].replace("*", "")
@@ -149,57 +206,232 @@ def calculo(REG,RAM,CACHE,INSTRUCOES):
 			break
 
 		INSTRUCOES[i] = INSTRUCOES[i].replace("*", "")
-		print('EXECUTOU')
+		if (branch == 0):
+			i +=1
+		else:
+			i = branch-1
+			branch = 0
 
-		
+def add(res,inst):
+	try:
+		reg1 = list(inst[4:6])
+		reg2 = list(inst[7:9])
+		reg3 = list(inst[10:])
+		valor1 = 0
+		valor2 = 0
+		valor3 = 0
+		if (reg1[0] == "$"):
+			valor1 = int(res[0][int(reg1[1])])
+		else:
+			valor1 = int(res[1][int(reg1[1])])
+		if (reg2[0] == "$"):
+			valor2 = int(res[0][int(reg2[1])])
+		else:
+			valor2 = int(res[1][int(reg2[1])])
+		if (reg3[0] == "$"):
+			valor3 = int(res[0][int(reg3[1])])
+		else:
+			valor3 = int(res[1][int(reg3[1])])
+	except:
+		print("Erro de formatação na instrução add!! (Verifique se há algum espaço no final)")
+		return 0
+	else:
+		valor1 = valor2 + valor3
+		if (reg1[0] == "$"):
+			res[0][int(reg1[1])] = str(valor1)
+		else:
+			res[1][int(reg1[1])] = str(valor1)
 
+	return res
 
+def addi(res,inst):
+	try:
+		reg1 = list(inst[5:7])
+		reg2 = list(inst[8:10])
+		reg3 = inst[11:]
+		valor1 = 0
+		valor2 = 0
+		valor3 = 0
+		if (reg1[0] == "$"):
+			valor1 = int(res[0][int(reg1[1])])
+		else:
+			valor1 = int(res[1][int(reg1[1])])
+		if (reg2[0] == "$"):
+			valor2 = int(res[0][int(reg2[1])])
+		else:
+			valor2 = int(res[1][int(reg2[1])])
+		valor3 = int(reg3)
+	except:
+		print("Erro de formatação da instrução addi!! (Verifique se há algum espaço no final)")
+		return 0
+	else:
+		valor1 = valor2 + valor3
+		if (reg1[0] == "$"):
+			res[0][int(reg1[1])] = str(valor1)
+		else:
+			res[1][int(reg1[1])] = str(valor1)
+	return res
 
+def lw(res,cache,inst):
+	try:
+		reg1 = list(inst[3:5])
+		imm = 0
+		reg2 = []
+		resto = list(inst[6:])
+		valor1 = 0
+		if (reg1[0] == "$"):
+			valor1 = int(res[0][int(reg1[1])])
+		else:
+			valor1 = int(res[1][int(reg1[1])])
+		if (len(resto) == 5):
+			imm == int(inst[6])
+			reg2.append(inst[8:10])
+		elif (len(resto) == 6):
+			imm == int(inst[6:8])
+			reg2.append(inst[10:12])
+		else:
+			print("Erro de formatação do lw, verifique se existe este endereço na CACHE ou se há espaço no final da instrução!")
+			return 0
+		reg2 = list(reg2[0])
+	except:
+		print("Erro de formatação do lw, verifique os espaços!")
+		return 0
+	else:
+		valor2 = 0
+		if (reg2[0] == "$"):
+			valor2 = int(res[0][int(reg2[1])])
+		else:
+			valor2 = int(res[1][int(reg2[1])])
 
+		try:
+			valor3 = valor2 + imm
+			valor = cache[valor3-1]
+		except:
+			print("Não existe esta posição de memória!!")
+			return 0
+		else:
+			if (reg1[0] == "$"):
+				res[0][int(reg1[1])] = str(valor)
+			else:
+				res[1][int(reg1[1])] = str(valor)
+			return res
 
+def sw(res,cache,inst):
+	try:
+		reg1 = list(inst[3:5])
+		imm = 0
+		reg2 = []
+		resto = list(inst[6:])
+		valor1 = 0
+		if (reg1[0] == "$"):
+			valor1 = int(res[0][int(reg1[1])])
+		else:
+			valor1 = int(res[1][int(reg1[1])])
+		if (len(resto) == 5):
+			imm == int(inst[6])
+			reg2.append(inst[8:10])
+		elif (len(resto) == 6):
+			imm == int(inst[6:8])
+			reg2.append(inst[10:12])
+		else:
+			print("Erro de formatação do lw, verifique se existe este endereço na CACHE ou se há espaço no final da instrução!")
+			return 0
+		reg2 = list(reg2[0])
+		valor2 = 0
+		if (reg2[0] == "$"):
+			valor2 = int(res[0][int(reg2[1])])
+		else:
+			valor2 = int(res[1][int(reg2[1])])
 
+	except:
+		print("Erro de formatação do lw, verifique os espaços!")
+		return 0
+	else:
+		try:
+			valor3 = valor2 + imm
+			cache[valor3-1] = valor1
+		except:
+			print("Não existe esta posição de memória!!")
+			return 0
+		else:
+			return res
 
+def j(inst,INSTRUCOES):
+	control = 0
+	branch = 0
+	for i in range(len(INSTRUCOES)):
+		if(INSTRUCOES[i][:-1] == inst[2:]):
+			branch = i+1
+			control = 1
+	if (control == 0):
+		return ("Erro")
+	else:
+		return (branch)
 
+def bne(inst,res,INSTRUCOES):
+	try:
+		reg1 = list(inst[4:6])
+		reg2 = list(inst[7:9])
+		label = inst[10:]
+		valor1 = 0
+		valor2 = 0
+		if (reg1[0] == "$"):
+			valor1 = int(res[0][int(reg1[1])])
+		else:
+			valor1 = int(res[1][int(reg1[1])])
+		if (reg2[0] == "$"):
+			valor2 = int(res[0][int(reg2[1])])
+		else:
+			valor2 = int(res[1][int(reg2[1])])
+	except:
+		print("Erro de formatação na instrução bne!! (verifique os espaços)")
+		return("Erro")
+	else:
+		if (valor1 != valor2):
+			control = 0
+			branch = 0
+			for i in range(len(INSTRUCOES)):
+				if(INSTRUCOES[i][:-1] == inst[10:]):
+					branch = i+1
+					control = 1
+			if (control == 0):
+				return ("Erro")
+			else:
+				return (branch)
+		else:
+			return 0
 
-
-
-def add(res):
-	print("Oi, Eu sou goku!")
-
-
-def addi(res):
-	print("Oi, Eu sou goku!")
-
-
-def lw(res):
-	print("Oi, Eu sou goku!")
-
-
-def j(res):
-	print("Oi, Eu sou goku!")
-
-
-def bne(res):
-	print("Oi, Eu sou goku!")
-
-
-def beq(res):
-	print("Oi, Eu sou goku!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def beq(inst,res,INSTRUCOES):
+	try:
+		reg1 = list(inst[4:6])
+		reg2 = list(inst[7:9])
+		label = inst[10:]
+		valor1 = 0
+		valor2 = 0
+		if (reg1[0] == "$"):
+			valor1 = int(res[0][int(reg1[1])])
+		else:
+			valor1 = int(res[1][int(reg1[1])])
+		if (reg2[0] == "$"):
+			valor2 = int(res[0][int(reg2[1])])
+		else:
+			valor2 = int(res[1][int(reg2[1])])
+	except:
+		print("Erro de formatação na instrução bne!! (verifique os espaços)")
+		return("Erro")
+	else:
+		if (valor1 == valor2):
+			control = 0
+			branch = 0
+			for i in range(len(INSTRUCOES)):
+				if(INSTRUCOES[i][:-1] == inst[10:]):
+					branch = i+1
+					control = 1
+			if (control == 0):
+				return ("Erro")
+			else:
+				return (branch)
+		else:
+			return 0
 
 main()
